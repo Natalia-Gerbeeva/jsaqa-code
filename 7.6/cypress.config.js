7444168fcabab3040082bee3158732a50dea945f
Nataliia <natalia.gerbeeva@gmail.com>
2026-07-10 20:13:56 +0300

    Add Cypress run scripts and test configuration

--- 7.6/cypress.config.js
+++ 7.6/cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'Cypress/cypress/e2e/**/*.cy.js',
+    supportFile: 'Cypress/cypress/support/e2e.js',
     viewportWidth: 1366,
     viewportHeight: 768,
   },
